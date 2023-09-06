984b25a804c3912b7b9c67bb4d97c39735be3e22
VinceNeede <v.bisogno3@studenti.unipi.it>
2023-09-06 19:34:14 +0200

    fit of gamma

--- suscettività/post_proc.py
+++ suscettività/post_proc.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 data=pd.read_csv('suscettività/susc.dat')
 
@@ -9,6 +10,13 @@ for ispin in range(3,20):
 
 plt.show()
 
+from scipy.optimize import curve_fit
+def mag_scaling(x,a,b):
+    return a*x**(-b)
 filtered_data = data[data['g'] == 1.0]
-plt.plot(filtered_data['L'], filtered_data['chi'])
+popt, pcov = curve_fit(mag_scaling,filtered_data['L'], np.abs(filtered_data['chi']),p0=[1.0, 7./4.])
+print(popt)
+x=np.linspace(3,20,100)
+plt.plot(filtered_data['L'], np.abs(filtered_data['chi']))
+#plt.plot(x,mag_scaling(x,*(popt)))
 plt.show()
