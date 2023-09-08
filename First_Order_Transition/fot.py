9d9428bfd59ad893f1a27be117723fe48229fb66
Quantum-Vixen <113099971+Quantum-Vixen@users.noreply.github.com>
2023-09-08 21:02:27 +0200

    more data

--- First_Order_Transition/fot.py
+++ First_Order_Transition/fot.py
@@ -10,10 +10,10 @@ sys.path.append("./")
 from Ising import Ising, Ising_Result
 
 gs=np.linspace(0.0,1.0,11)
-hs_pos=np.logspace(start= np.log10(0.03), stop= np.log10(0.0002), num=16+1)
+hs_pos=np.logspace(start= np.log10(0.03), stop= np.log10(0.0002), num=16)
 hs_neg=-hs_pos
 hs=np.sort(np.concatenate((hs_pos,hs_neg)))
-Ls=range(12,21)
+Ls=range(3,21)
 
 def long_mag(L: int,g: np.double,h: np.double) -> np.double:
     res=Ising(L,g,h,True,False)
@@ -30,8 +30,8 @@ else:
 
 
 for L in tqdm(Ls, desc= "L", position=0):
-    for g in tqdm(gs, desc="outer_loop", position=1, leave=False):
-        for h in tqdm(hs, desc="inner_loop", position=2, leave=False):
+    for g in tqdm(gs, desc="g", position=1, leave=False):
+        for h in tqdm(hs, desc="h", position=2, leave=False):
             long_mag_result=long_mag(L,g,h)
             fout.write(str(L) +"," + str(g) + "," + str(h) + "," + str(long_mag_result) + "\n")
         fout.flush()
