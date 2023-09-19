f4dc957f85e390ebd3a4714bfc1e22e7af77c236
VinceNeede <v.bisogno3@studenti.unipi.it>
2023-09-19 16:10:06 +0200

    Updated magnetization and gap for universal function

--- Magnetization and gap/automatic_sampling.py
+++ Magnetization and gap/automatic_sampling.py
@@ -1,60 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
-def norm_pdf (x, mu=1.0, sigma=0.5):
-    return 1/(sigma * np.sqrt(2 * np.pi))*np.exp( - (x - mu)**2 / (2 * sigma**2))
-    
-start=0.
-end=2.
-npoints=20
-
-fileout="fixed_g_1.out"
-
-x=np.linspace(start, end, npoints)
-fx=norm_pdf(x)
-g=0
-density=(1+g*(fx-1))/fx
-x_density=np.cumsum(density)
-x_density-=x_density.min()
-x_density/=x_density.max()
-x_density*=(end-start)+start
-if 1. not in x_density:
-    x_density=np.append(x_density,1.)
-    x_density=np.sort(x_density)
-# print(x)
-# print(x_density)
-
-
-# fx_density = norm_pdf(x_density)
-
-
-# plt.plot(x,x_density,'ok',ms = 10,label = 'linear')
-# # plt.plot(x_density,fx_density,'or',ms = 10,label = 'warped')
-# # plt.legend(loc = 'upper right')
-# plt.show()
-
-import subprocess
-import os
-
-if os.path.isfile(fileout):
-    fout=open(fileout,"a")
-else:
-    fout=open(fileout,"w")
-    fout.write("L,g,E_p0,E_p1,E_p2,E_m0,E_m1,E_m2,Mx\n")
-gfield=1.
-for ispin in range(23,24):
-    f=open("chain.in", "w")
-    f.write(
-f"{ispin}	    ! Number of spins in the system		(ell)\n\
-{gfield}d0	! Transverse magnetic field strength	(g)\n\
-0.d0	! Longitudinal magnetic field strength	(h)\n\
-.true.	! Type of boundary conditions  		(.true. -> PBC,        .false. -> OBC)\n\
-.true.	! Type of diagonalization      		(.true. -> Davidson,   .false. -> Lapack full diag) NOT IMPLEMENTED\n"\
-        )
-    f.close()
-    p=subprocess.Popen("./_results/Ising_Parity", shell=True, stdout=subprocess.PIPE)
-    out=p.communicate()[0].decode()
-    #if (out!=""):
-    #    print(ispin, gfield, out)
-    fout.write(out)
-fout.close()
+import sys
+from tqdm import tqdm
+sys.path.append('./')
+from Ising import Ising, Ising_Parity_Result
+ 
+elles=range(19,21)
+kgs=np.linspace(-1.,1.,10)
+if 0. not in kgs:
+    kgs=np.append(kgs,0.)
+    kgs=np.sort(kgs)
+f=open("Magnetization and gap/chain.out","a")
+#f.write("L,kg,E_m,E_p,broken_mag\n")
+for ell in tqdm(elles):
+    for kg in tqdm(kgs,leave=False):
+        g=kg/ell+1.
+        res=Ising(ell,g,0.,True,True)
+        f.write(f"{ell},{kg},{res.E_m[0]},{res.E_p[0]},{res.broken_mag}\n")
+    f.flush()
+f.close()
+        
+        
