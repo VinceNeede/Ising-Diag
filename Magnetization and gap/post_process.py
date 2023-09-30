c565d9e7b9997ad9d47ae82e00fcc506c87cb501
VinceNeede <v.bisogno3@studenti.unipi.it>
2023-09-30 10:39:28 +0200

    images

--- Magnetization and gap/post_process.py
+++ Magnetization and gap/post_process.py
@@ -2,15 +2,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data=pd.read_csv("Magnetization and gap/chain.out")
-fig, axs = plt.subplots(2)
+plt.rcParams.update({'font.size': 22, 'figure.figsize':[11, 8]})
+data=pd.read_csv("Magnetization and gap/chain_2.out")
+figs=[None for _ in range(3)]
+axs=[None for _ in range(3)]
+for i in range(3):
+    figs[i],axs[i]=plt.subplots()
 for ispin in range(3,21):
     filter_data=data[data['L']==ispin]
-    axs[0].plot(filter_data['kg'], abs(filter_data['broken_mag'])*ispin**0.125,label=f"L= {ispin}")
-    axs[1].plot(filter_data['kg']/ispin+1.,abs(filter_data['E_m']-filter_data['E_p'])*ispin)
-fig.legend()
-axs[0].set_ylabel(r'$M L^{1/8}$')
-#axs[0].set_xlim((-1.,1.))
-axs[1].set_ylabel(r'$\Delta L$')
-axs[1].set_xlabel(r'$k_g=(g-g_c)L$')
-plt.show()
+    filter_data=filter_data.sort_values(by='kg')
+    axs[0].plot(filter_data['kg'], abs(filter_data['broken_mag'])*ispin**(0.125),label=f"L= {ispin}",linewidth=1.5)
+    axs[1].plot(filter_data['g'], abs(filter_data['broken_mag']),label=f"L= {ispin}",linewidth=1.5)
+    axs[2].plot(filter_data['g'],abs(filter_data['E_m']-filter_data['E_p']),label=f"L= {ispin}",linewidth=1.5)
+axs[0].set_ylabel(r'$m L^{1/8}$')
+axs[1].set_ylabel(r'$m')
+axs[2].set_ylabel(r'$\Delta$')
+axs[0].set_xlabel(r'$(g-1)L$')
+axs[1].set_xlabel(r'$g$')
+axs[2].set_xlabel(r'$g$')
+axs[0].set_xlim((-1.,1.))
+
+figs[0].savefig("Magnetization and gap/universal_fun.png")
+figs[1].savefig("Magnetization and gap/magn.png")
+figs[2].savefig("Magnetization and gap/gap.png")
--- transverse susceptance/post_proc_2.py
+++ transverse susceptance/post_proc_2.py
@@ -1,36 +1,36 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+plt.rcParams.update({'font.size': 22, 'figure.figsize':[11, 8]})
 
 data=pd.read_csv('transverse susceptance/susc2.dat')
-
-for ispin in range(3,21):
+figs=[None for _ in range(2)]
+axs=[None for _ in range(2)]
+for i in range(2):
+    figs[i],axs[i]=plt.subplots()
+elles=[2*i+2 for i in range(1,10)]
+for ispin in elles:
     filtered_data = data[data['L'] == ispin]
     filtered_data=filtered_data.sort_values(by='g')
-    plt.plot(filtered_data['g'], filtered_data['chi']*filtered_data['g'])
+    axs[0].plot(filtered_data['g'], filtered_data['chi']*filtered_data['g'],label=f"L={ispin}",linewidth=2.5)
+axs[0].set_xlabel(r'$g$')
+axs[0].set_ylabel(r'$g\chi_x$')
+axs[0].legend(loc="upper right",framealpha=0.,ncol=1)
+axs[0].set_xticks(np.arange(0., 2.5, step=0.5))
+figs[0].savefig("transverse susceptance/specific_heat.png")
 
-plt.show()
 filtered_data = data[data['g'] == 1.]
 filtered_data=filtered_data.sort_values(by='L')
-plt.scatter(filtered_data['L'], filtered_data['chi'])
-x=np.linspace(3,20,10,endpoint=True)
+axs[1].scatter(filtered_data['L'], filtered_data['chi'],color='red',marker='*',label="Lanczos")
+axs[1].set_xlabel(r'$L$')
+axs[1].set_ylabel(r'$g\chi_x(g=1)$')
+x=np.linspace(1,100,10,endpoint=True)
 def fun (x):
     return 0.32*np.log(x)+0.16
-plt.semilogx(x,fun(x))
+axs[1].semilogx()
+axs[1].set_xticks([1,10,100],[r'$1$',r'$10$',r'$100$'])
+axs[1].plot(x,fun(x),label=r'$0.32\log (L) + 0.16$')
+axs[1].legend(loc="upper left",framealpha=0.,ncol=1)
+
+figs[1].savefig("transverse susceptance/specific_heat_scaling.png")
 plt.show()
-
-# from scipy.optimize import curve_fit
-# #data=pd.read_csv('transverse susceptance/susc_fixed_g.dat')
-# filtered_data = data[data['g'] == 1.]
-
-# def mag_scaling(x,a,b):
-#     return b*np.log(x)+a
-
-# popt, pcov = curve_fit(mag_scaling,filtered_data['L'], filtered_data['chi'],p0=[1.0, 7./4.])
-# print(popt)
-# x=np.linspace(3,20,100)
-# plt.semilogx()
-# plt.plot(filtered_data['L'], filtered_data['chi'])
-# plt.plot(x,mag_scaling(x,*(popt)))
-# #plt.plot(x,mag_scaling(x,*(popt)))
-# plt.show()
